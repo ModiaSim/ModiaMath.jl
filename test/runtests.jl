2d9a5147c0cc6df7635cac3076f5aa8b707bcc2a
Chris Laughman <crlaugh@gmail.com>
2018-08-23 10:13:58 -0400

    More modifications to get Travis to work.

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,10 +7,10 @@ else
 end
 
 @testset "Test ModiaMath" begin
-#    include(joinpath("result", "_includes.jl"))
+    include(joinpath("result", "_includes.jl"))
     include(joinpath("variables", "_includes.jl"))
-#    include(joinpath("frames", "_includes.jl"))
-#    include(joinpath("simulation", "_includes.jl"))
+    include(joinpath("frames", "_includes.jl"))
+    include(joinpath("simulation", "_includes.jl"))
 end
 
 end
