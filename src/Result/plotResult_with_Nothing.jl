f80fb2c18fb138279534bbb0179c49c9dbb0fe56
Martin.Otter@dlr.de <Martin.Otter@dlr.de>
2018-09-18 19:38:58 +0200

    Added new file to git

---
+++ src/Result/plotResult_with_Nothing.jl
@@ -0,0 +1,18 @@
+# License for this file: MIT (expat)
+# Copyright 2017-2018, DLR Institute of System Dynamics and Control
+#
+# This file is part of module 
+#   ModiaMath.Result (ModiaMath/Result/_module.jl)
+#
+
+function plot(result, names::Symbol; heading::AbstractString="", grid::Bool=true, xAxis=:time, figure::Int=1) 
+    println("... ModiaMath.plot(..): Call is ignored, since PyPlot not installed in current environment.")
+end
+
+function plot(result, names::Tuple; heading::AbstractString="", grid::Bool=true, xAxis=:time, figure::Int=1) 
+    println("... ModiaMath.plot(..): Call is ignored, since PyPlot not installed in current environment.")
+end
+
+function plot(result, names::AbstractMatrix; heading::AbstractString="", grid::Bool=true, xAxis=:time, figure::Int=1) 
+    println("... ModiaMath.plot(..): Call is ignored, since PyPlot not installed in current environment.")
+end
