6efcb7317d108fc585f4547fed41dcbc75a19ceb
Martin.Otter@dlr.de <Martin.Otter@dlr.de>
2018-12-02 14:57:44 +0100

    Version changed to 0.2.6; Project.toml/Manifest.toml files and documentation adapted

--- src/ModiaMath.jl
+++ src/ModiaMath.jl
@@ -66,7 +66,7 @@ Absolute path of package directory of ModiaMath
 """
 const path = dirname(dirname(@__FILE__))   # Absolute path of package directory
 const Time = Float64   # Prepare for later Integer type of time
-const Version = "0.2.6-dev from 2018-11-25 10:16"
+const Version = "0.2.6 from 2018-12-02 14:35"
 
 println(" \nImporting ModiaMath version ", Version)
 
